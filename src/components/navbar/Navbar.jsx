2671bd1d0057881821edb73e422f239c0c4637b5
917sympathies <maraturazbakhtin@yandex.ru>
2024-01-12 20:40:42 +0400

    Создана база для Sidebar'а и Navbar'а

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import styles from "./style.module.css"
+
+const Navbar = () => {
+    return <div className={styles.navbar_container}>
+        Navbar
+    </div>
+}
+
+export default Navbar;
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,7 @@
+import styles from "./style.module.css"
+
+const Sidebar = () =>{
+    return <div className={styles.sidebar_container}>Sidebar</div>
+}
+
+export default Sidebar;
