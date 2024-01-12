fca18ff758e25ba28e1d0769c4ceaa5e98ffd180
917sympathies <maraturazbakhtin@yandex.ru>
2024-01-12 20:37:57 +0400

    Начальная настройка

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/navbar/Navbar"
+import Sidebar from "../../components/sidebar/Sidebar"
+
+export default function DashboardPage(){
+    return(
+        <div>
+            <Navbar/>
+            <Sidebar/>
+            Dashboard
+        </div>
+    )
+}
