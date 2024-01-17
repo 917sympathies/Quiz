615c8956e82f5d3973f6172d8063489c0ce4a3b0
917sympathies <maraturazbakhtin@yandex.ru>
2024-01-17 12:03:58 +0400

    Основная верстка игрового лобби dashboard'а

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,49 @@
+'use client'
+import React, {useState} from 'react'
 import Navbar from "../../components/navbar/Navbar"
 import Sidebar from "../../components/sidebar/Sidebar"
 import styles from "./style.module.css"
 
+const QuestionPacksMock = [
+    {id: 1, name: "Animals"},
+    {id: 2, name: "Cars"},
+    {id: 3, name: "Movies"}
+]
+
+
 export default function DashboardPage(){
+    const [selectedQuestionPack, setSelectedQuestionPack] = useState("")
+
+    const handleStartGame = () => {
+        console.log(`You've selected quiz pack ${selectedQuestionPack}. Game start button was pressed!`);
+    }
+
     return(
         <div>
             <Navbar/>
             <div className={styles.dashboard_container}>
-                <h1 className={styles.dashboard_content}>Dashboard</h1>
+                <div className={styles.dashboard_content}>
+                    <span className={styles.dashboard_lobby_label}>GAME LOBBY</span>
+                    <div className={styles.dashboard_lobby_container}>
+                        <div className={styles.dashboard_lobby_players_container}>
+                            <span style={{fontSize: "48px"}}>PLAYERS</span>
+                            <div className={styles.dashboard_lobby_playerslist}>
+                                LIST
+                            </div>
+                        </div>
+                        <div className={styles.dashboard_lobby_questionpack_container}>
+                            <span style={{fontSize: "48px"}}>QUIZ PACK</span>
+                            <select onChange={(e) => setSelectedQuestionPack(e.target.value)}>
+                                {QuestionPacksMock.map(qp => {
+                                    return <option value={qp.name}>{qp.name}</option>
+                                })}
+                            </select>
+                            <div className={styles.dashboard_lobby_startbtn} onClick={() => handleStartGame()}>
+                                <span>START</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <Sidebar/>
             </div>
         </div>
